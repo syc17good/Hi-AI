eca254331f13ffc124b3c2b52a0137bec7a296d5
奇焱 <oncwnuIbLyI_fMnUZ9Oc2SHbITxk@git.weixin.qq.com>
2020-05-02 17:58:24 +0800

    图像安全审核

---
+++ cloudfunctions/ai/index.js
@@ -0,0 +1,33 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const extCi = require("@cloudbase/extension-ci");
+const tcb = require("tcb-admin-node");
+
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+tcb.init({
+  env: "my-env-id"
+});
+tcb.registerExtension(extCi);  
+// 云函数入口函数
+exports.main = async (event, context) => {
+  // console.log('ai-------------', event.path)
+
+  try {
+    const opts = {
+      type: "porn,terrorist,politics"
+    }
+    const res = await tcb.invokeExtension('CloudInfinite', {
+      action: 'DetectType',
+      cloudPath: event.path, // 需要分析的图像的绝对路径，与tcb.uploadFile中一致
+      operations: opts
+    })
+    
+    // console.log(JSON.stringify(res.data, null, 4));
+    return res.data
+  } catch (err) {
+    
+    console.log(JSON.stringify(err, null, 4));
+  }
+}
